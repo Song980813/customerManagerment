8c14df630f6e58a4ed2b86c10ac506eb9bef6480
SongShuai <1773818087@qq.com>
2020-09-03 20:47:17 +0800

    @Song项目9-3提交

--- src/main/java/com/huayu/management/controller/TbAftersaleController.java
+++ src/main/java/com/huayu/management/controller/TbAftersaleController.java
@@ -1,9 +1,16 @@
 package com.huayu.management.controller;
 
 
+import com.huayu.management.bo.LayuiEntity;
+import com.huayu.management.entity.TbAftersale;
+import com.huayu.management.service.ITbAftersaleService;
+import com.huayu.management.service.ITbEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/management/tb-aftersale")
 public class TbAftersaleController {
+    @Autowired
+    ITbAftersaleService aftersaleService;
+
+    @RequestMapping("selectall")
+    public LayuiEntity queryAll(){
+        List<TbAftersale> list=aftersaleService.list();
+        LayuiEntity layuiEntity= new LayuiEntity();
+        layuiEntity.setCode(0);
+        layuiEntity.setMsg("查询成功");
+        layuiEntity.setCount(list.size());
+        layuiEntity.setData(list);
+        return layuiEntity;
+    }
 
 }
